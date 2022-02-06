cce6417d6a9991a4ac8a8812a060707d23c2e2b8
rb990 <rb990@cam.ac.uk>
2022-02-06 21:37:40 +0000

    finished1d

--- Task1D.py
+++ Task1D.py
@@ -2,6 +2,6 @@ from floodsystem.geo import rivers_with_station, stations_by_river
 from floodsystem.stationdata import build_station_list
 stations = build_station_list()
 print(len(rivers_with_station(stations)))
-#print("The stations by River Aire are: " + str(sorted(stations_by_river(stations)['River Aire'])))
-#print("The stations by River Cam are: " + str(sorted(stations_by_river(stations)['River Cam'])))
-#print("The stations by River Thames are: " + str(sorted(stations_by_river(stations)['River Thames'])))
+print("The stations by River Aire are: " + str(sorted(stations_by_river(stations)['River Aire'])))
+print("The stations by River Cam are: " + str(sorted(stations_by_river(stations)['River Cam'])))
+print("The stations by River Thames are: " + str(sorted(stations_by_river(stations)['River Thames'])))
