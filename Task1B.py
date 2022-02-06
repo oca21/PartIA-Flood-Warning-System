4ee010e38d35ea6e7c3beba047c847dac7345acc
rb990 <rb990@cam.ac.uk>
2022-02-06 18:12:44 +0000

    Done task1B

---
+++ Task1B.py
@@ -0,0 +1,15 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+stations = build_station_list()
+
+closest = []
+farthest = []
+sorted_stations = stations_by_distance(stations, (52.2053, 0.1218))
+for i in range(len(sorted_stations)):
+    if i < 10:
+        closest.append((sorted_stations[i][0].name, sorted_stations[i][0].town, sorted_stations[i][1]))
+    if i > len(sorted_stations) - 10:
+        farthest.append((sorted_stations[i][0].name, sorted_stations[i][0].town, sorted_stations[i][1]))
+
+print(closest)
+print(farthest)
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,3 +10,17 @@ from .utils import sorted_by_key  # noqa
 
 def hi():
     pass
+
+#------------------------------------------------#
+###TASK 1B###
+from haversine import haversine, Unit
+def stations_by_distance(stations, p):
+    lists=[]
+    for i in range(len(stations)):
+        distance=haversine(stations[i].coord, p)
+        lists.append((stations[i], distance))
+    return sorted_by_key(lists, 1, reverse=False)
+    
+
+
+#------------------------------------------------#
