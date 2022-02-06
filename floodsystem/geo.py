37979e22047a6e797d97ed2c63792163cd9bbf88
Olivia Ambler <oca21@cam.ac.uk>
2022-02-06 21:25:11 +0000

    Completed task1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -2,29 +2,27 @@
 geographical data.
 """
 from .utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
 #------------------------------------------------#
 ###TASK 1B###
 from haversine import haversine, Unit
 def stations_by_distance(stations, p):
     lists=[]
-    for i in range(len(stations)):
-        distance=haversine(stations[i].coord, p)
-        lists.append((stations[i], distance))
-    return sorted_by_key(lists, 1, reverse=False)
+    for i in range(len(stations)): #For each station
+        distance=haversine(stations[i].coord, p) #Calculate distance of the station from the coord p
+        lists.append((stations[i], distance)) #Add this to list of tuples of the form (station, distance)
+    return sorted_by_key(lists, 1, reverse=False) #Return the sorted list based off the distance from p
 #------------------------------------------------#
-pass
 ###TASK1C###
 def stations_within_radius(stations, centre, r):
+    stations_within_r=[]
 
-     # Build list of stations
-    stations = build_station_list()
+    stations_with_distance = stations_by_distance(stations, centre) #Get stations and distance from centre 
+    for i in range(len(stations_with_distance)):
+        if stations_with_distance[i][1]<r:
+            stations_within_r.append(stations_with_distance[i][0].name)
+    return stations_within_r
 
-#------------------------------------------------# 
-    for station in stations:
-        if station.name in [
-
-        ]:
-            print(station)
 
 #------------------------------------------------#
 ###TASK 1D###
