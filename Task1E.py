36c4f55a6f9d1b37b819760b65a40130bd67fe5e
rb990 <rb990@cam.ac.uk>
2022-02-06 22:32:30 +0000

    finished 1E

--- Task1E.py
+++ Task1E.py
@@ -1,5 +1,4 @@
 from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list
 stations = build_station_list()
-
 print(rivers_by_station_number(stations, N=10))
