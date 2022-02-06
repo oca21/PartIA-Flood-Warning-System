5c9412c45e0e5b7e8dbbd31bdb82fa476ca452ba
Olivia Ambler <oca21@cam.ac.uk>
2022-02-06 18:18:40 +0000

    Started 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,20 +7,18 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from floodsystem.stationdata import build_station_list
 
 def hi():
     pass
 
-#------------------------------------------------#
-###TASK 1B###
-from haversine import haversine, Unit
-def stations_by_distance(stations, p):
-    lists=[]
-    for i in range(len(stations)):
-        distance=haversine(stations[i].coord, p)
-        lists.append((stations[i], distance))
-    return sorted_by_key(lists, 1, reverse=False)
-    
+def stations_within_radius(stations, centre, r):
 
+     # Build list of stations
+    stations = build_station_list()
 
-#------------------------------------------------#
+    for station in stations:
+        if station.name in [
+
+        ]:
+            print(station)
