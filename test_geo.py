ab9e045bf5740b9f07a21743922afb6e23b085ee
rb990 <rb990@cam.ac.uk>
2022-02-08 15:12:18 +0000

    finished testing for task1e

--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,5 @@
 #TESTING GEO.PY#
-from floodsystem.geo import stations_by_distance, rivers_with_station, stations_by_river
+from floodsystem.geo import stations_by_distance, rivers_with_station, stations_by_river, rivers_by_station_number
 from floodsystem.station import MonitoringStation
 
 #Test Task 1B
@@ -44,7 +44,6 @@ def test_stations_by_distance():
     Test3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
     stations=[Test1, Test2, Test3]
-
 
     TestResult=stations_by_distance(stations, (0, 0))
     assert (TestResult[0][0], TestResult[1][0], TestResult[2][0]) == (Test2, Test3, Test1)
@@ -114,6 +113,46 @@ def test_rivers_with_stations():
     Test3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
     stations=[Test1, Test2, Test3]
+
+
     assert rivers_with_station(stations) == {'River 3', 'River Thames'}
 
 #Test1C
+
+#Test Task 1E
+#-----------------------------------------------------
+def test_rivers_by_station_number():
+    s_id = "testSid1"
+    m_id = "testMid1"
+    label = "TS1"
+    coord = (1.0, 4.0)
+    trange = (-2, 5)
+    river = "River Thames"
+    town = "My Town 1"
+    Test1 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    s_id = "testSid2"
+    m_id = "testMid2"
+    label = "TS2"
+    coord = (0.0, 1.0)
+    trange = (-2, 2)
+    river = "River Thames"
+    town = "Town 2"
+    Test2 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    s_id = "testSid3"
+    m_id = "testMid3"
+    label = "TS3"
+    coord = (1.0, 1.0)
+    trange = (-2, 2)
+    river = "River 3"
+    town = "Town 3"
+    Test3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    stations=[Test1, Test2, Test3]
+
+    assert rivers_by_station_number(stations,2)[0][0] == "River Thames"
+    assert rivers_by_station_number(stations,2)[0][1] == 2
+    assert rivers_by_station_number(stations,2)[1][0] == "River 3"
+    assert rivers_by_station_number(stations,2)[1][1] == 1
+test_rivers_by_station_number()
