402d113a282c26b06f49a428114e78f018c1bc42
rb990 <rb990@cam.ac.uk>
2022-02-08 14:53:30 +0000

    finished test for 1D

--- test_geo.py
+++ test_geo.py
@@ -1,9 +1,20 @@
 #TESTING GEO.PY#
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_distance, rivers_with_station, stations_by_river
 from floodsystem.station import MonitoringStation
 
-#TEST TASK 1B
-#------------------------------------------------
+#Test Task 1B
+#-----------------------------------------------------
+#Test haversine function
+def test_haversine():
+    from haversine import haversine, Unit
+    Coord1=(0.0,0.0)
+    Coord2=(0.1,7.0)
+    Coord3=(1.0,2.0)
+    assert haversine(Coord1, Coord1)==0
+    assert round(haversine(Coord2, Coord1), 1)==778.4
+    assert round(haversine(Coord3, Coord1),1)==248.6
+
+#test stations_by_distance
 def test_stations_by_distance():
     s_id = "testSid1"
     m_id = "testMid1"
@@ -33,11 +44,15 @@ def test_stations_by_distance():
     Test3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
     stations=[Test1, Test2, Test3]
+
+
     TestResult=stations_by_distance(stations, (0, 0))
     assert (TestResult[0][0], TestResult[1][0], TestResult[2][0]) == (Test2, Test3, Test1)
 
-#TEST RIVERS WITH STATIONS
-def test_rivers_with_station():
+#Test Task 1D
+#-----------------------------------------------------
+#test stations_by_river
+def test_stations_by_river():
     s_id = "testSid1"
     m_id = "testMid1"
     label = "TS1"
@@ -65,5 +80,41 @@ def test_rivers_with_station():
     town = "Town 3"
     Test3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
 
+    stations=[Test1, Test2, Test3]
 
-#rivers_with_station, stations_by_river
+    assert sorted(stations_by_river(stations)['River Thames']) == [Test1.name, Test2.name]
+
+#test rivers_with_stations
+def test_rivers_with_stations():
+    s_id = "testSid1"
+    m_id = "testMid1"
+    label = "TS1"
+    coord = (1.0, 4.0)
+    trange = (-2, 5)
+    river = "River Thames"
+    town = "My Town 1"
+    Test1 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    s_id = "testSid2"
+    m_id = "testMid2"
+    label = "TS2"
+    coord = (0.0, 1.0)
+    trange = (-2, 2)
+    river = "River Thames"
+    town = "Town 2"
+    Test2 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    s_id = "testSid3"
+    m_id = "testMid3"
+    label = "TS3"
+    coord = (1.0, 1.0)
+    trange = (-2, 2)
+    river = "River 3"
+    town = "Town 3"
+    Test3 = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    stations=[Test1, Test2, Test3]
+    assert rivers_with_station(stations) == {'River 3', 'River Thames'}
+
+#Test Task 1D
+#-----------------------------------------------------
